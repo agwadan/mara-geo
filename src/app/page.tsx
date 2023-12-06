6500690b897e6b789a6e1bbe187bc202cb5fd37c
agwadan <agwa.dan@gmail.com>
2023-12-06 23:01:34 +0300

    Add humburger menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Head from "next/head";
 import Hero from "../components/LandingPage/Hero";
 import About from "../components/LandingPage/About";
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
+  const [humbergerOpen, setHumbergerOpen] = useState(true);
+
   return (
-    <div className="navbar z-10 !important">
-      <nav className=" fixed flex flex-row bg- justify-between items-center bg-primaryGrey w-screen">
+    <nav className="navbar z-10 !important">
+      <div className=" fixed flex flex-row bg- justify-between items-center bg-primaryGrey w-full py-y px-6">
         <Image
           src="./images/logo_main.svg"
           alt="Mara Geo"
@@ -12,14 +17,58 @@ const Navbar = () => {
           className="py-3"
         />
 
-        <ul className="flex flex-row  list-none w-100">
+        <ul className="flex flex-row md:hidden list-none w-100 ">
           <li className="px-2 cursor-pointer hover:text-secondary">Home</li>
           <li className="px-2 cursor-pointer hover:text-secondary">About</li>
           <li className="px-2 cursor-pointer hover:text-secondary">Services</li>
+          <li className="px-2 cursor-pointer hover:text-secondary">
+            Testimonials
+          </li>
           <li className="px-2 cursor-pointer hover:text-secondary">Contact</li>
         </ul>
-      </nav>
-    </div>
+
+        {/* Humberger Icon */}
+        <div
+          className="hidden md:block cursor-pointer"
+          onClick={() => setHumbergerOpen(true)}
+        >
+          <GiHamburgerMenu />
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      <div className={`${humbergerOpen ? "block" : "hidden"}`}>
+        <div className="fixed  h-screen w-full bg-black bg-opacity-60">
+          <div
+            id="menu"
+            className="absolute flex flex-col items-left py-8 space-y-6 font-800 bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-xl"
+          >
+            <button
+              className="w-full ml-80"
+              onClick={() => setHumbergerOpen(false)}
+            >
+              <RxCross2 />
+            </button>
+
+            <a className="pl-6 border-t border-primaryGrey pt-4" href="#">
+              Home
+            </a>
+            <a className="pl-6 border-t border-primaryGrey pt-4" href="#About">
+              About
+            </a>
+            <a className="pl-6 border-t border-primaryGrey pt-4" href="#">
+              Our Services
+            </a>
+            <a className="pl-6 border-t border-primaryGrey pt-4" href="#">
+              Tools
+            </a>
+            <a className="pl-6 border-t border-primaryGrey pt-4" href="#">
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
